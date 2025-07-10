2c854da29149ad898695e03306a7861d8377fe76
SUGAVANESHWARAN S <154986511+Sugavanesh17@users.noreply.github.com>
2025-07-10 20:35:08 +0530

    wrong file

--- app/components/Error.js
+++
@@ -1,7 +0,0 @@
-export default function Error({ message }) {
-  return (
-    <div className="container mx-auto px-4 py-6 text-center text-red-600 font-semibold">
-      <p>Error: {message || "Something went wrong."}</p>
-    </div>
-  );
-}
--- app/components/Loading.js
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="container mx-auto px-4 py-6 text-center text-gray-600">
-      <p>Loading...</p>
-    </div>
-  );
-}
